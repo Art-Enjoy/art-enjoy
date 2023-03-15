e6504d17e30f3bdf47c4626faab8a56d35d7d45d
JenniferCorderoR <jennifer.factoriaf5@gmail.com~>
2023-03-15 11:03:35 +0100

    added footer in root

--- src/router/Root.jsx
+++ src/router/Root.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import NavbarAdmin from "../components/NavbarAdmin";
+import Footer from "../components/FooterSesion";
 
 import HomeUser from "../pages/HomeUser"
 function Root() {
@@ -8,6 +9,8 @@ function Root() {
             <NavbarAdmin />
             <HomeUser/>
             <Outlet />
+            <Footer />
+            
 
             
           
