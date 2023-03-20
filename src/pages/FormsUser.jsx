dc7ee3d896dd4f4b40904fc87d1f1e2027b0a499
JenniferCorderoR <jennifer.factoriaf5@gmail.com~>
2023-03-20 13:58:14 +0100

    added formsuser

--- src/pages/FormsUser.jsx
+++ src/pages/FormsUser.jsx
@@ -1,54 +1,96 @@
 import React from 'react'
-
-const Forms = () => {
-  
-
-  return (
-  <div className="container-form">
-    <form onSubmit={handleSubmit} itemID="form1">
-      <h1>Comprar entradas</h1>
-<div className="mb-3">
-<label htmlFor="title" className="form-label">Tipo de entrada</label>
-<input name="title" type="text" className="form-control" placeholder="Adult@/Niñ@" onChange={handleTitleChange} required/>
-</div>
-<div className="mb-3">
-<label htmlFor="price" className="form-label">Cantidad</label>
-<input name="price" type="text" className="form-control" placeholder="Unidades..." onChange={handlePriceChange} required/>
-</div>
-<div className="mb-3">
-<label className="form-label" htmlFor="description">Fecha</label>
-<input name="description" type="text" className="form-control" id="--/--/----" required placeholder="Tell us about the product" onChange={handleDescriptionChange}/>
-</div>
-<div className="mb-3">
-<label htmlFor="user" className="form-label">Hora</label>
-<input name="user" type="text" className="form-control" required placeholder="--:--" onChange={handleUserChange} />
-</div>
-<div className="mb-3">
-<label htmlFor="units" className="form-label">Nombre y apellidos</label>
-<input name="units" type="number" min="1" max="25" className="form-control" required placeholder="Nombre/Apellidos" onChange={handleUnitsChange} />
-</div>
-<div className="mb-3">
-<label htmlFor="location" className="form-label">Teléfono</label>
-<input name="location" type="text" className="form-control" required placeholder="+34---------" onChange={handleLocationChange} />
-</div>
-<div className="mb-3">
-<label htmlFor="location" className="form-label">DNI</label>
-<input name="location" type="text" className="form-control" required placeholder="--------X" onChange={handleLocationChange} />
-</div>
-<div className="mb-3">
-<label htmlFor="location" className="form-label">Email</label>
-<input name="location" type="text" className="form-control" required placeholder="Introducir Email" onChange={handleLocationChange} />
-</div>
-<div className="mb-3">
-<label htmlFor="location" className="form-label">Método de pago</label>
-<input name="location" type="text" className="form-control" required placeholder="Tarjeta de crédito / Paypal" onChange={handleLocationChange} />
-</div>
-
-<button type="submit" className="btn btn-primary" id="btn-ad">Comprar</button>
-</form>
-</div>
-
-)
+import { Component } from 'react'
+import { Container } from 'react-bootstrap'
+export default class FormsUser extends Component {
+  render() {
+    return (
+      <>
+      <Container>
+      <form>
+        <h3>Comprar entradas</h3>
+        <div className="mb-3">
+          <label>Tipo de entrada</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Adult@/Niñ@"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Cantidad</label>
+          <input
+            type="number"
+            className="form-control"
+            placeholder="Unidades..."
+          />
+        </div>
+        <div className="mb-3">
+          <label>Fecha</label>
+          <input
+            type="number"
+            className="form-control"
+            placeholder="--/--/----"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Hora</label>
+          <input
+            type="number"
+            className="form-control"
+            placeholder="--:--"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Nombre y apellidos</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Nombre/Apellidos"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Teléfono</label>
+          <input
+            type="number"
+            className="form-control"
+            placeholder="+34---------"
+          />
+        </div>
+        <div className="mb-3">
+          <label>DNI</label>
+          <input
+            type="tnumber"
+            className="form-control"
+            placeholder="--------X"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Email </label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Ingrese email"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Método de pago</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Tarjeta de crédito / Paypal"
+          />
+        </div>
+        <div className="d-grid">
+        <div class="col col-lg-2">
+          <button type="submit" className="btn btn-dark">
+            Comprar
+          </button>
+          </div>
+        </div>
+      </form>
+      </Container>
+      </>
+    )
+  }
 }
 
-export default Forms;
