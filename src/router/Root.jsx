7392bfcb482bb33ddf61de8bba1a54b252cb777e
JenniferCorderoR <jennifer.factoriaf5@gmail.com~>
2023-03-21 09:21:39 +0100

    fixed

--- src/router/Root.jsx
+++ src/router/Root.jsx
@@ -2,18 +2,13 @@ import { Outlet } from "react-router-dom";
 import NavbarAdmin from "../components/NavbarAdmin";
 import Footer from "../components/Footer";
 
-import HomeUser from "../pages/HomeUser"
 function Root() {
     return (
         <>
             <NavbarAdmin />
-            <HomeUser/>
             <Outlet />
             <Footer />
-            
-
-            
-          
+      
         </>
     )
 }
