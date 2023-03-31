9fbdc5e5f9d00d348fb1f5753b8d3f314d10f991
JenniferCorderoR <jennifer.factoriaf5@gmail.com~>
2023-03-31 14:10:02 +0200

    fixed base problem

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
-base: "/art-enjoy/",
 plugins: [react()],
 test: {
 environment: "jsdom",
